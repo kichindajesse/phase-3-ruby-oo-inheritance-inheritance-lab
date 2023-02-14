f5adb77977a70ec43facad10d2fc52171299718d
kichinda <53686544+kichinda@users.noreply.github.com>
2023-02-14 20:30:04 +0300

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,28 @@
-class Student 
-
+class User 
+  attr_accessor :first_name, :last_name
 end
+
+class Student < User
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
+end
+
+
+
+
